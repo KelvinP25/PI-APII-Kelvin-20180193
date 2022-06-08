dd0fd0f75ff0953899ddd97da1ca8e3ad3918e58
KelvinP25 <kelvincastillo9911@gmail.com>
2022-06-08 19:26:39 -0400

    Completando el Registro de Prestamos

--- app/src/main/java/com/example/pi_api_kelvin_20180193/ui/theme/Deudor/RegistroPrestamosScreen.kt
+++ app/src/main/java/com/example/pi_api_kelvin_20180193/ui/theme/Deudor/RegistroPrestamosScreen.kt
@@ -1,26 +1,54 @@
 package com.example.pi_api_kelvin_20180193.ui.theme.Deudor
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
 @Composable
-fun RegistroPrestamosScreen(navHostController: NavHostController) {
+fun RegistroPrestamosScreen(
+    navHostController: NavHostController,
+    viewModel: DeudorviewModel = hiltViewModel()
+) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Registro") })
+            TopAppBar(title = { Text(text = "Registo Prestamos") })
         },
     ) {
         Column(Modifier.padding(16.dp)) {
-            OutlinedButton(onClick = { navHostController.navigate("ConsultaNv") }) {
+            OutlinedTextField(
+                value = viewModel.nombre,
+                onValueChange = { viewModel.nombre = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Nombre") },
+                leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = null) }
+            )
+            OutlinedTextField(
+                value = viewModel.concepto,
+                onValueChange = { viewModel.concepto = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Nombre") },
+                leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = null) }
+            )
+            OutlinedTextField(
+                value = viewModel.monto.toString(),
+                onValueChange = { viewModel.monto = it.toDouble() },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Nombre") },
+                leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = null) }
+            )
+            OutlinedButton(onClick = {
+                viewModel.Guardar()
+                navHostController.navigate("ConsultaNv")
+            }) {
                 Text(text = "Guardar")
             }
         }
