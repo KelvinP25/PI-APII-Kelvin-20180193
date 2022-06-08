a1c37a676705b402c980bd36072888d7fc3a3c60
KelvinP25 <kelvincastillo9911@gmail.com>
2022-06-08 15:31:18 -0400

    Creando navegacion del la Consulta

--- app/src/main/java/com/example/pi_api_kelvin_20180193/ui/theme/entities/Consulta.kt
+++ app/src/main/java/com/example/pi_api_kelvin_20180193/ui/theme/entities/Consulta.kt
@@ -4,7 +4,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 
 @Composable
