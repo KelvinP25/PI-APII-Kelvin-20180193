81f84ff068057f19ee0b6514f75e88a359ea50cb
KelvinP25 <kelvincastillo9911@gmail.com>
2022-06-08 16:38:01 -0400

    Terminando la Navegacion

--- app/src/main/java/com/example/pi_api_kelvin_20180193/MainActivity.kt
+++ app/src/main/java/com/example/pi_api_kelvin_20180193/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.pi_api_kelvin_20180193.ui.theme.PIAPIKelvin20180193Theme
 import com.example.pi_api_kelvin_20180193.ui.theme.entities.Consulta
 import com.example.pi_api_kelvin_20180193.ui.theme.entities.Registro
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +30,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun MyApp(){
-
-
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         val navHostController = rememberNavController()
-        NavHost(navController = navHostController, startDestination = "Consulta"){
+
+        NavHost(navController = navHostController, startDestination = "ConsultaNv"){
             composable(route = "ConsultaNv") {
                 Consulta(navHostController)
             }
@@ -46,13 +46,5 @@ fun MyApp(){
                 Registro(navHostController)
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PIAPIKelvin20180193Theme {
-        MyApp()
     }
 }
