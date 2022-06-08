0e19eb743d4e3199dac7e4af5eacad812d7be981
KelvinP25 <kelvincastillo9911@gmail.com>
2022-06-08 19:09:15 -0400

    Creando la funcion FowDeudor en el MainActivity

--- app/src/main/java/com/example/pi_api_kelvin_20180193/MainActivity.kt
+++ app/src/main/java/com/example/pi_api_kelvin_20180193/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.pi_api_kelvin_20180193
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pi_api_kelvin_20180193.model.Deudor
 import com.example.pi_api_kelvin_20180193.ui.theme.PIAPIKelvin20180193Theme
 import com.example.pi_api_kelvin_20180193.ui.theme.Deudor.ConsultaPrestamosScreen
 import com.example.pi_api_kelvin_20180193.ui.theme.Deudor.RegistroPrestamosScreen
@@ -44,5 +47,14 @@ fun MyApp(){
                 RegistroPrestamosScreen(navHostController)
             }
         }
+    }
+}
+
+@Composable
+fun RowDeudor(deudor: Deudor){
+    Row{
+        Text(text = deudor.nombre)
+        Text(text = deudor.concepto)
+        Text(text = deudor.monto.toString())
     }
 }
